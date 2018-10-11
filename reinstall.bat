67ffcc58fdc60b4100284eb4d8cb9bbc07c89cce
etnpce <etnpce@users.noreply.github.com>
2018-10-11 16:28:40 -0600

    Create Functioning Windows pyinstaller bat
    Can take a folder argument to search for pyinstaller if it isn't on path. 
    Prevents strange pyinstaller issue by removing dist/standfire_mini_interface in advance
    Deletes troublesome dll after pyinstaller finishes.

--- reinstall.bat
+++ reinstall.bat
@@ -1,7 +1,34 @@
+@SETLOCAL
+
 cd "%~dp0"
-pyinstaller standfire_mini_interface.py -y ^
-    --hidden-import="pandas._libs.skiplist" ^
-    --hidden-import="pandas._libs.tslibs.np_datetime" ^
-    --hidden-import="pandas._libs.tslibs.nattype" ^
-    -i "sf_icon_64.ico"
-for %f in (bin, data, pyfvs) do xcopy %f dist/standfire_mini_interface/ /E /I
+
+@echo off
+
+SET INSTNAME=pyinstaller.exe
+for %%N in (%INSTNAME%) do SET INSTPATH=%%~$PATH:N
+IF EXIST "%~f1\%INSTNAME%" SET INSTPATH="%~f1\%INSTNAME%"
+IF EXIST "%~dp1\%INSTNAME%" SET INSTPATH="%~dp1\%INSTNAME%"
+
+IF NOT EXIST %INSTPATH% do goto err
+
+echo on
+
+@REM The next line is required because pyinstaller crashes when recreating the folder directly after deleting it
+@REM I have no clue why.  Maybe a weird buffered IO quirk
+rmdir "dist/standfire_mini_interface" /S /Q
+
+%INSTPATH% standfire_mini_interface.py -y ^
+--hidden-import="pandas._libs.skiplist" ^
+--hidden-import="pandas._libs.tslibs.np_datetime" ^
+--hidden-import="pandas._libs.tslibs.nattype" ^
+--hidden-import="pandas._libs.tslibs.timedeltas" ^
+-i "sf_icon_64.ico"
+
+del "dist\standfire_mini_interface\sqlite3.dll"
+
+@for %%f in (bin, data, pyfvs) do xcopy %%f "dist/standfire_mini_interface/%%f" /S /E /I /Q
+
+@goto :eof
+
+:err
+echo %INSTNAME% not found. Add it to the path, or pass a path to it as an argument.
